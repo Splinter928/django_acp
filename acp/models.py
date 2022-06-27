7603b6e50a5c0f84c132eece4a4ca3fa5bef7e44
Your Name <splinter928@gmail.com>
2022-06-27 22:55:02 +0500

    commit 27_06_2022_1

--- acp/models.py
+++ acp/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+
+class Partition(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+    avail = models.CharField(max_length=10)
+    nodes_count = models.IntegerField()
+    nodes_status = models.CharField(max_length=100)
+    cpus_status = models.CharField(max_length=100)
+    small_nodes_list = models.TextField()
+    all_nodes_list = models.TextField()
+
+    def __str__(self):
+        """Returns string representation of the model"""
+        return str(self.name) + ':' + str(self.avail)
